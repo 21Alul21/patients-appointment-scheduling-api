0ad0c14a7cdc509b5bb9d3cdfbf1f736dce12cd2
Augustine Alul <117871941+21Alul21@users.noreply.github.com>
2025-06-16 11:50:12 +0100

    Update DoctorEntity.java

--- AppointmentManagement/src/main/java/com/example/AppointmentManagement/Entities/DoctorEntity.java
+++ AppointmentManagement/src/main/java/com/example/AppointmentManagement/Entities/DoctorEntity.java
@@ -9,14 +9,18 @@ public class DoctorEntity{
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID DoctorID;
+    @NotBlank(message = "first name cannot be blank")
     private String firstName;
+    @NotBlank(message = "last name cannot be blank")
     private String lastNmame;
+    @Size(min = 5, message = "length of password should be gretater than {min}")
     private String Password;
     private Date dateOfBirth;
     private String phoneNumber;
     private int notification;
     private String meansOfIdentification;
     private String idNumber;
+    @Email(message = "please enter a valid email address")
     private String email;
     @Enumerated(EnumType.STRING)
     private SpecializationEnum specialization;
