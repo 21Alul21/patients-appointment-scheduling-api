d074fa6c21512c59ff15fcecdfc71cac23e1ee74
Augustine Alul <117871941+21Alul21@users.noreply.github.com>
2025-06-16 12:01:51 +0100

    Update AdminEntity.java

--- AppointmentManagement/src/main/java/com/example/AppointmentManagement/Entities/AdminEntity.java
+++ AppointmentManagement/src/main/java/com/example/AppointmentManagement/Entities/AdminEntity.java
@@ -7,7 +7,9 @@ public class AdminEntity{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int adminID;
+    @NotBlank(message = "please enter your admin username")
     private String username;
+    @NotBlank("password cannot be blank")
     private String password;
     private DateTime dateAccountCreated;
     private String role;
