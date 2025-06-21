7b034be4b1110d88ccfd0bbc8b9c193db69c33b3
Augustine Alul <117871941+21Alul21@users.noreply.github.com>
2025-06-21 11:19:51 +0100

    Update DoctorDTO.java

--- AppointmentManagement/src/main/java/com/example/AppointmentManagement/DTOs/DoctorDTO.java
+++ AppointmentManagement/src/main/java/com/example/AppointmentManagement/DTOs/DoctorDTO.java
@@ -1,1 +1,23 @@
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class DoctorEntity{
+    private UUID DoctorId;
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private String email;
+    private SpecializationEnum specialization;
+    private LocalDateTime registeredAt;
+
+  public enum SpecializationEnum{
+    GYNECOLOGIST,
+    DENTIST,
+    OPTICIAN,
+    GENERAL_MEDICAL_SERVICES
+}
+
+}
+
+
