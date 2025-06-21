1b42e809876359615255839e6d2e2e4a8f115269
Augustine Alul <117871941+21Alul21@users.noreply.github.com>
2025-06-21 23:40:53 +0100

    Update PatientService.java

--- AppointmentManagement/src/main/java/com/example/AppointmentManagement/Services/PatientService.java
+++ AppointmentManagement/src/main/java/com/example/AppointmentManagement/Services/PatientService.java
@@ -1,1 +1,17 @@
 
+@Service
+public class PatientService{
+  private final PatientRepository patientRepository;
+  private final PateintMapper PatientMapper;
+  
+  public PatientService(PatienRepository patientRepository, PatientDTO patientDTO, DTOMapper dtoMapper){
+    this.patientRepository = patientRepository;
+  }
+
+  public List<PatientDTO> getPatients(){
+
+  List<PatientDTO> patientDTO = patientRepository.findAll()
+ .stream().map(patientEntity->patientMapper.toDTO(patientEntity)).collect(Collectors.toList());
+    return patientDTO;
+  }
+}
