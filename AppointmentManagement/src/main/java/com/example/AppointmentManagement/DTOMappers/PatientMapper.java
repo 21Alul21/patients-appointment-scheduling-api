7521258158e3f1174cdb46b762da238d61ce1f8a
Augustine Alul <117871941+21Alul21@users.noreply.github.com>
2025-06-21 14:00:57 +0100

    Update PatientMapper.java

--- AppointmentManagement/src/main/java/com/example/AppointmentManagement/DTOMappers/PatientMapper.java
+++ AppointmentManagement/src/main/java/com/example/AppointmentManagement/DTOMappers/PatientMapper.java
@@ -1,1 +1,19 @@
+public class PatientMapper {
 
+    public PatientDTO toDTO(PatientEntity patientEntity) {
+        if (patientEntity == null) {
+            return null;
+        }
+
+        PatientDTO patientDTO = new PatientDTO();
+        patientDTO.setPatientId(patientEntity.getId());
+        patientDTO.setFirstName(patientEntity.getFirstName());
+        patientDTO.setLastName(patientEntity.getLastName());
+        patientDTO.setEmail(patientEntity.getEmail());
+        patientDTO.setPhoneNumber(patientEntity.getPhoneNumber());
+        patientDTO.setGender(patientEntity.getGender());
+        patientDTO.setRegisteredAt(patientEntity.getRegisteredAt());
+
+        return patientDTO;
+    }
+}
