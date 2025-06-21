2f2fbbe9c0bdf2931ef2bc6c836479b54964c09a
Augustine Alul <117871941+21Alul21@users.noreply.github.com>
2025-06-21 11:04:03 +0100

    Update PatientDTO.java

--- AppointmentManagement/src/main/java/com/example/AppointmentManagement/DTOs/PatientDTO.java
+++ AppointmentManagement/src/main/java/com/example/AppointmentManagement/DTOs/PatientDTO.java
@@ -1,1 +1,12 @@
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PatientDTO{
+    private UUID patientID;
+    private String firstName;
+    private String lastNmame;
+    private String email;
+    private LocalDateTime registeredAt;
+    
+}
