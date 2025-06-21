a7c24321f5b98b4f83764e64f39cf83cf91d1e1b
Augustine Alul <117871941+21Alul21@users.noreply.github.com>
2025-06-21 09:19:15 +0100

    Update AppointmentEntity.java

--- AppointmentManagement/src/main/java/com/example/AppointmentManagement/Entities/AppointmentEntity.java
+++ AppointmentManagement/src/main/java/com/example/AppointmentManagement/Entities/AppointmentEntity.java
@@ -1,68 +1,51 @@
-
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class AppointmentEntity{
+public class AppointmentEntity {
+
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
-    private UUID apointmentID;
-    @NotBlank(message = "please enter your appointment title")
-    private String apointmentTitle;
-    private String apointmentDescription;
-    @NotBlank(message = "please enter your first name")
-    private String patientFirstName;
-    @NotBlank(message = "please enter your last name")
-    private String patientLastNmame;
-    @NotBlank("please enter your appointment date and time")
-    private DateTime appointmentDueDate;
-    private DateTime appointmentBookedAt;
+    private UUID appointmentID;
+
+    @NotBlank(message = "Please enter your appointment title")
+    private String appointmentTitle;
+
+    private String appointmentDescription;
+
+    @NotNull(message = "Please enter your appointment date and time")
+    private LocalDateTime appointmentDueDate;
+
+    private LocalDateTime appointmentBookedAt;
+
     @Enumerated(EnumType.STRING)
     private AppointmentStatusEnum appointmentStatus;
+
     @Enumerated(EnumType.STRING)
     private SpecializationEnum specialization;
-    @Enumerated(EnumType.STRING)
-    private DoctorFirstNameEnum doctorFirstName;
-    @Enumerated(EnumType.STRING)
-    private DoctorLastnameEnum doctorLastName;
-    
+
     @OneToMany(mappedBy = "appointmentEntity", cascade = CascadeType.ALL, orphanRemoval = true)
-    List<NotificationEntity> notificationEntity = new ArrayList<>();
-    
+    private List<NotificationEntity> notificationEntity = new ArrayList<>();
+
     @ManyToOne
-    @joinColumn(name = "patient_id")
+    @JoinColumn(name = "patient_id")
     private PatientEntity patientEntity;
 
     @ManyToOne
     @JoinColumn(name = "doctor_id")
     private DoctorEntity doctorEntity;
-    
 
-// Enums
-    public Enum AppointmentStatusEnum{
-    PENDING,
-    BOOKED,
-    DECLINED,
-    RESCHEDULED
-}
+    public enum AppointmentStatusEnum {
+        PENDING,
+        BOOKED,
+        DECLINED,
+        RESCHEDULED
+    }
 
-
-public Enum DoctorFirstNameEnum{
-
-}
-
-public Enum DoctorLastNameEnum{
-
-}
-
-
-public Enum SpecializationEnum{
-    GYNACOLOGIST,
-    DENTIST,
-    OPTICIAN,
-    GENERAL_MEDICAL_SERVICES
-}
-
-
-
+    public enum SpecializationEnum {
+        GYNECOLOGIST,
+        DENTIST,
+        OPTICIAN,
+        GENERAL_MEDICAL_SERVICES
+    }
 }
