53a3bf5932e8bf859b9d574e39f7c1b3b6434240
Augustine Alul <117871941+21Alul21@users.noreply.github.com>
2025-06-23 09:10:08 +0100

    Create PatientController.java

---
+++ AppointmentManagement/src/main/java/com/example/AppointmentManagement/Controllers/PatientController.java
@@ -0,0 +1,15 @@
+@RestController
+@RequestMapping("/patients") // Corrected the typo
+public class PatientController {
+
+    private final PatientService patientService;
+
+    public PatientController(PatientService patientService) {
+        this.patientService = patientService;
+    }
+
+    @GetMapping("/{patientId}")
+    public ResponseEntity<PatientDTO> getPatient(@PathVariable UUID patientId) {
+        return ResponseEntity.ok(patientService.getPatient(patientId));
+    }
+}
