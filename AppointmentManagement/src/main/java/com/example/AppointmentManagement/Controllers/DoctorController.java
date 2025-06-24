27ca272593837de5b8ca9b7e6d360ad7325ed9d0
Augustine Alul <117871941+21Alul21@users.noreply.github.com>
2025-06-24 22:48:25 +0100

    Update DoctorController.java

--- AppointmentManagement/src/main/java/com/example/AppointmentManagement/Controllers/DoctorController.java
+++ AppointmentManagement/src/main/java/com/example/AppointmentManagement/Controllers/DoctorController.java
@@ -1,1 +1,8 @@
 
+@RestController
+@RequestMapping("/doctor")
+public class DoctorController{
+  @GetMapping("/doctor/{doctorId}")
+  public DoctorDTO getDoctor(@PathVariable UUID doctorId){
+  }
+}
