54416d40e86b3229167725452a4a41edf73921fd
Augustine Alul <117871941+21Alul21@users.noreply.github.com>
2025-06-26 22:10:51 +0100

    Update DoctorController.java

--- AppointmentManagement/src/main/java/com/example/AppointmentManagement/Controllers/DoctorController.java
+++ AppointmentManagement/src/main/java/com/example/AppointmentManagement/Controllers/DoctorController.java
@@ -2,8 +2,27 @@
 @RestController
 @RequestMapping("/doctor")
 public class DoctorController{
-  @GetMapping("/doctor/{doctorId}")
+  private final DoctorService doctorService;
+ 
+  public DoctorController(DoctorService doctorService){
+    this.doctorService = doctorService;
+  }
+  @GetMapping("/{doctorId}")
   public ResponseEntity<DoctorDTO> getDoctor(@PathVariable UUID doctorId){
     return ResponseEntity.ok(DoctorService.getDoctor(doctorId));
   }
+
+  @GetMapping("/doctor")
+  public ResponseEntity<List<DocrorDTO>> getDoctors(){
+    return ResponseEntity.ok(doctorService.getDoctors());
+  }
+
+  @PostMapping("/register-doctor")
+  public ResponseEntity<DoctorEntity> registerDoctor(DoctorEntity doctorEntity){
+    return ResponseEntity.ok(doctorService.createDoctor(doctorEntity));
+  }
+
+  @PatchMapping("/update-profile")
+  
+
 }
