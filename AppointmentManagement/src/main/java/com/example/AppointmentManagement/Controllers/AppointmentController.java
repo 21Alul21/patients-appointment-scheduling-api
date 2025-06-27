109de7f9ad99dfdf56da305198df18e497d6eb28
Augustine Alul <117871941+21Alul21@users.noreply.github.com>
2025-06-27 08:11:03 +0100

    Update AppointmentController.java

--- AppointmentManagement/src/main/java/com/example/AppointmentManagement/Controllers/AppointmentController.java
+++ AppointmentManagement/src/main/java/com/example/AppointmentManagement/Controllers/AppointmentController.java
@@ -12,7 +12,12 @@ public class AppointmentController{
   @PostMapping("/create-appointment")
   public ResponseEntity<AppointmentEntity> createAppointment(@RequestBody AppointmentEntity appointmentEntity){
    createdAppointment = appointmentService.createAppointment(appointmentEntity);
-   notificationService.createNotification();
+   NotificationEntity notificationEntity = new NotificationEntity();
+   notificationEntity.setMessage("new appointment created");
+   notificationEntity.setSentAt(new LocalDate().now());
+   notificationEntity.setIsRead("false");
+   notificationEntity.setStatus("PENDING");
+   notificationService.createNotification(notificationEntity);
     return ResponseEntity
     .status(HttpStatus.CREATED)
     .build();
