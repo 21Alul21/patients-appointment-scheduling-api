88de69e3e780ea611dceabbc82e11dc1783db263
Augustine Alul <117871941+21Alul21@users.noreply.github.com>
2025-06-27 07:46:40 +0100

    Update AppointmentController.java

--- AppointmentManagement/src/main/java/com/example/AppointmentManagement/Controllers/AppointmentController.java
+++ AppointmentManagement/src/main/java/com/example/AppointmentManagement/Controllers/AppointmentController.java
@@ -1,1 +1,20 @@
 
+@RestController
+@RequestMapping("/appointment")
+public class AppointmentController{
+
+  private final AppointmentService appointmentService;
+  private final NotificationService notificationService;
+  public AppointmentController(AppointmentService appointmentService, NotificationService notificationService){
+  this.appointmentService = appointmentService;
+  }
+
+  @PostMapping("/create-appointment")
+  public ResponseEntity<AppointmentEntity> createAppointment(@RequestBody AppointmentEntity appointmentEntity){
+   createdAppointment = appointmentService.createAppointment(appointmentEntity);
+   notificationService.createNotification();
+    return ResponseEntity
+    .status(HttpStatus.CREATED)
+    .build();
+  }
+}
