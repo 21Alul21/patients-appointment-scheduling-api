5cb385088115e1556d063a32fa2dd9a9bd3a1364
Augustine Alul <117871941+21Alul21@users.noreply.github.com>
2025-06-30 00:28:30 +0100

    Update DoctorService.java

--- AppointmentManagement/src/main/java/com/example/AppointmentManagement/Services/DoctorService.java
+++ AppointmentManagement/src/main/java/com/example/AppointmentManagement/Services/DoctorService.java
@@ -1,1 +1,19 @@
 
+@Service
+public class DoctorService{
+
+  private final DoctorRepository doctorRepository;
+  public DoctorService(DoctorRepository doctorRepository){
+  this.doctorService = doctorService;
+  }
+
+  public DoctorDTO getDoctor(UUID doctorId){
+   DoctorEntity doctor = doctorRepository
+     .findById(doctorId)
+     .orElseThrow(() -> new DoctorNotFoundException("no doctor was found at the given Id"));
+    return doctorDTO.toDTO(doctor);
+  }
+
+  public List<DoctorDTO> getDoctors(){
+  }
+}
