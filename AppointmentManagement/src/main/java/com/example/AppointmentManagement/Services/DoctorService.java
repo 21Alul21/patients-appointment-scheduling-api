974555bf4b140807df7561052df44a5c26d1bcb2
Augustine Alul <117871941+21Alul21@users.noreply.github.com>
2025-06-30 00:48:43 +0100

    Update DoctorService.java

--- AppointmentManagement/src/main/java/com/example/AppointmentManagement/Services/DoctorService.java
+++ AppointmentManagement/src/main/java/com/example/AppointmentManagement/Services/DoctorService.java
@@ -3,17 +3,27 @@
 public class DoctorService{
 
   private final DoctorRepository doctorRepository;
-  public DoctorService(DoctorRepository doctorRepository){
+  private final DoctorMapper doctorMapper;
+  
+  public DoctorService(DoctorRepository doctorRepository, DoctorMapper doctorMapper){
   this.doctorService = doctorService;
+  this.doctorMapper = doctorMapper;
   }
 
   public DoctorDTO getDoctor(UUID doctorId){
    DoctorEntity doctor = doctorRepository
      .findById(doctorId)
      .orElseThrow(() -> new DoctorNotFoundException("no doctor was found at the given Id"));
-    return doctorDTO.toDTO(doctor);
+    return doctorMapper.toDTO(doctor);
   }
 
   public List<DoctorDTO> getDoctors(){
+    List<DoctorEntity> doctors = doctorService.findAll();
+    if (doctors.isEmpty){
+      throw new DoctorsNotFoundException("doctors not found");
+    }
+      return
+      .map(doctorMapper::toDTO)
+      .collect(Collectors.toList())
   }
 }
