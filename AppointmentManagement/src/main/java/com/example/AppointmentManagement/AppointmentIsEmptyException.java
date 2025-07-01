79316172885e10b35bb43e0ecc9af9727b0ff7d5
Augustine Alul <117871941+21Alul21@users.noreply.github.com>
2025-07-01 17:28:30 +0100

    Create AppointmentIsEmptyException.java
    Added the code for AppointmentIsEmpty custom exception handler

---
+++ AppointmentManagement/src/main/java/com/example/AppointmentManagement/AppointmentIsEmptyException.java
@@ -0,0 +1,9 @@
+public class AppointmentIsEmptyException{
+   public AppointmentIsEmptyException(String message){
+    super(message);
+   }
+
+  public AppointmentIsEmptyException(String message, Throwable cause){
+   super(message, cause);
+  }
+}
