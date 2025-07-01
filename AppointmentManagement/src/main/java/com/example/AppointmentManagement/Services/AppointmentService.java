b52c1b580cf1a87c81897a83bd9a61b24b176ca0
Augustine Alul <117871941+21Alul21@users.noreply.github.com>
2025-07-01 08:28:34 +0100

    Update AppointmentService.java

--- AppointmentManagement/src/main/java/com/example/AppointmentManagement/Services/AppointmentService.java
+++ AppointmentManagement/src/main/java/com/example/AppointmentManagement/Services/AppointmentService.java
@@ -1,1 +1,15 @@
 
+@Service
+public class AppointmentService{
+
+  private final AppointmentRepository appointmentRepository;
+
+  public AppointmentService(AppointmentRepository appointmentRepository){
+  this.appointmentRepository = appointmentRepository;
+  }
+
+  public List<AppointmentEntity> getAppointments(){
+    return appointmentRepository.findAll()
+      .orElseThrow(() -> new AppointmentNotFoundException("no appointments were found"));
+  }
+}
