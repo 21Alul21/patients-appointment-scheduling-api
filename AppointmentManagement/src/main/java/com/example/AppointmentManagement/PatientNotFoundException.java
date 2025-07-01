d812b86491b7f4aabcd8129e0a182c40aafc76f8
Augustine Alul <117871941+21Alul21@users.noreply.github.com>
2025-07-01 20:06:33 +0100

    Create PatientNotFoundException.java

---
+++ AppointmentManagement/src/main/java/com/example/AppointmentManagement/PatientNotFoundException.java
@@ -0,0 +1,9 @@
+public class PatientNotFoundException{
+  public PatientNotFoundException(String message){
+   super(message);
+  }
+  
+  public PatientNotFoundException(String message, Throwable cause){
+   super(message, cause)
+  }
+}
