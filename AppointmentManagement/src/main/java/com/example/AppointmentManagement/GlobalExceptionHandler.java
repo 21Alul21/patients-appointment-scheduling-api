9f1dc374246ec0f3a1c99129007d2d70698d5245
Augustine Alul <117871941+21Alul21@users.noreply.github.com>
2025-07-01 23:13:27 +0100

    Update GlobalExceptionHandler.java

--- AppointmentManagement/src/main/java/com/example/AppointmentManagement/GlobalExceptionHandler.java
+++ AppointmentManagement/src/main/java/com/example/AppointmentManagement/GlobalExceptionHandler.java
@@ -2,8 +2,13 @@
 @RestControllerAdvice
 public class GlobalExceptionHandler{
  @ExceptionHandler(AppointmentNotFoundException.class)
- public ResponseEntity<?> handleAppointmentNotFoud(AppointmentNotFoundException ex){
+ public ResponseEntity<?> handleAppointmentNotFound(AppointmentNotFoundException ex){
    return new ResponseEntity<>(createErrorBody(ex.getMessage()), HttpSatus.BAD_REQUEST)
+ }
+
+ @ExceptionHandler(DoctorNotFoundException.class)
+ public ResponseEntity<?> handleDoctorNotFound(DoctorNotFoundException ex){
+   return new ResponseEntity<>(createErrorBody(ex.getMessage()), HttpStatus.BAD_REQUEST)
  }
 }
   
