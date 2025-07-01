4d115f4c2062f861e949a89eceda6d158bf21b19
Augustine Alul <117871941+21Alul21@users.noreply.github.com>
2025-07-01 19:20:10 +0100

    Update NotificationService.java

--- AppointmentManagement/src/main/java/com/example/AppointmentManagement/Services/NotificationService.java
+++ AppointmentManagement/src/main/java/com/example/AppointmentManagement/Services/NotificationService.java
@@ -6,7 +6,9 @@ private final NotificationRepository notificationRepository;
 piblic NotificationService(NotificationService notificationService){
  this.notificationRepository = notificationRepository;
 }
-    
+
+ 
+// CREATE notification for a booked appointment     
 public NotificationEntity createNotification(NotificationEntity notificationEntity){
 if (notificationEntity == null){
 throw new IllegalArgumentException("empty notification");
