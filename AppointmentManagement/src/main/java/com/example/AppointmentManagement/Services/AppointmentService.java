a188360e0a6bc7eb32e92d45c57aeb05accbd567
Augustine Alul <117871941+21Alul21@users.noreply.github.com>
2025-07-01 12:50:30 +0100

    Update AppointmentService.java

--- AppointmentManagement/src/main/java/com/example/AppointmentManagement/Services/AppointmentService.java
+++ AppointmentManagement/src/main/java/com/example/AppointmentManagement/Services/AppointmentService.java
@@ -18,23 +18,35 @@ public class AppointmentService{
      .orElseThrow(() -> new AppointmentNotFoundException("no appointment was found at id: " + appointmentId));
   }
 
-  public AppointmentEntity createAppointment(AppointmentEntiry appointmentEntity){
-   if (appointmentEntity == null){
-     throw new AppointmentIsEmptyException("you cannot create an empty appointment");
-   }
-  return appointmentRepository.save(appointmentEntity);
-  }
+   // CREATE appointment
+    public AppointmentEntity createAppointment(AppointmentEntity appointmentEntity) {
+        if (appointmentEntity == null) {
+            throw new AppointmentIsEmptyException("You cannot create an empty appointment");
+        }
+        return appointmentRepository.save(appointmentEntity);
+    }
 
-  public AppointmentEntity updateAppointment(AppointmentEntity appointmentEntity, UUID appointmentId){
-   if (appointmentEntity == null){
-     throw new AppointmentIsEmptyException("cannot update appointment with empty data fields");
-   }
-    AppointmentEntity existingAppointment = appointmentRepisitory
-      .findById(appointmentId)
-      .orElseThrow(() -> new AppointmentNotFoundException("cannot find appointment record to update"));
-      existingAppointment.set(appointmentEntity.get(appointmentStatus));
-      existingAppointment.set(appointmentEntity.get(appointmentDueDate));
-      appointmentRepository.save(existingAppointment);
+    // UPDATE appointment
+    public AppointmentEntity updateAppointment(AppointmentEntity appointmentEntity, UUID appointmentId) {
+        if (appointmentEntity == null) {
+            throw new AppointmentIsEmptyException("Cannot update appointment with empty data fields");
+        }
+
+        AppointmentEntity existingAppointment = appointmentRepository
+                .findById(appointmentId)
+                .orElseThrow(() ->
+                        new AppointmentNotFoundException("Cannot find appointment record to update with ID: " + appointmentId));
+
+        existingAppointment.setAppointmentStatus(appointmentEntity.getAppointmentStatus());
+        existingAppointment.setAppointmentDueDate(appointmentEntity.getAppointmentDueDate());
+
+        return appointmentRepository.save(existingAppointment);
+    }
+
+  public void deleteAppointment(Long appointmentId){
+   AppointmentEntity appontment = appointmentRepository
+     .findById.orElseThrow(() -> new AppointmentNotFoundException("cannot find the appointment to delete"));
+    appointmentRepository.deletebyId(appointmentId);
   }
 
 }
