a707f646146343142e26e7ef3ae82734f5f96745
Augustine Alul <117871941+21Alul21@users.noreply.github.com>
2025-07-02 13:03:21 +0100

    Update OrganizationsEntity.java

--- AppointmentManagement/src/main/java/com/example/AppointmentManagement/Entities/OrganizationsEntity.java
+++ AppointmentManagement/src/main/java/com/example/AppointmentManagement/Entities/OrganizationsEntity.java
@@ -1,16 +1,29 @@
-
 @Entity
 @Data
-@NoArgConstructor
+@NoArgsConstructor
 @AllArgsConstructor
-public class OrganizationEntity{
-  @Id
-  @GeneratedValue(strategy = GenerationType.UUID)
-  private UUID organizationId; 
-  private String organizationName;
-  @Enumerated(enumType.STRING)
-  private String subscriptionStatus;
-  private LocalDateTime registeredAt;
-  
+public class OrganizationEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID organizationId;
+
+    private String organizationName;
+
+    @Enumerated(EnumType.STRING)
+    private SubscriptionStatus subscriptionStatus;
+
+    private LocalDateTime registeredAt;
+
+    @OneToMany(mappedBy = "organization", cascade = CascadeType.ALL)
+    private List<DoctorEntity> doctors;
+
+    @OneToMany(mappedBy = "organization", cascade = CascadeType.ALL)
+    private List<PatientEntity> patients;
+
+    @OneToMany(mappedBy = "organization", cascade = CascadeType.ALL)
+    private List<AppointmentEntity> appointments;
+
+    @OneToMany(mappedBy = "organization", cascade = CascadeType.ALL)
+    private List<NotificationEntity> notifications;
 }
-  
