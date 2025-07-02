8289d444d3e3172cb56d30361a346d696370aaaa
Augustine Alul <117871941+21Alul21@users.noreply.github.com>
2025-07-02 09:42:42 +0100

    Update GlobalExceptionHandler.java

--- AppointmentManagement/src/main/java/com/example/AppointmentManagement/GlobalExceptionHandler.java
+++ AppointmentManagement/src/main/java/com/example/AppointmentManagement/GlobalExceptionHandler.java
@@ -16,13 +16,15 @@ public class GlobalExceptionHandler{
    return new ResponseEntity<>(createdErrorBody(ex.getMessage()), HttpStatus.BAD_REQUEST);
  }
 
- @ExceptionHandler(AppointmentNotFoundException.class)
- public ResponseEntity<?> handleAppointmentNotFound(AppointmentNotFoundException ex){
-    return new ResponseEntity<>(createErrorBody(ex.getMessage), HttpStatus.BAD_REQUEST);
+ @ExceptionHandler(AppointmentIsEmptyException.class)
+ public ResponseEntity<?> handle appointmentIsEmpty(AppointmentIsEmptyException ex){
+  return new ResponseEntity<>(createErrorBody(ex.getMessage()), HttpStatus.BAD_REQUEST); 
+ }
+
+  @ExceptionHandler(RuntimeException.class)
+ public ResponseEntity<?> handleAppointmentNotFound(RuntimeException ex){
+    return new ResponseEntity<>(createErrorBody("a runtime exception has occured", HttpStatus.BAD_REQUEST);
   }
-
- @ExceptionHandler()
-
  
 }
   
