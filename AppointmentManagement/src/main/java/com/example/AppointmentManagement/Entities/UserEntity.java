f4311718500080c7d9134ba22bcdcc2d64d35fe4
Augustine Alul <117871941+21Alul21@users.noreply.github.com>
2025-07-05 20:07:37 +0100

    Create UserEntity.java

---
+++ AppointmentManagement/src/main/java/com/example/AppointmentManagement/Entities/UserEntity.java
@@ -0,0 +1,11 @@
+
+  @Entity
+  @NoArgsConstructor
+  @AllArgsConstructor
+  @Data
+  public class UserEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID userId;
+    private 
+  }
