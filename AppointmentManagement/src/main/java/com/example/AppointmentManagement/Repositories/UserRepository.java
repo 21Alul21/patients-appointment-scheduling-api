b357b37f3fbdf10cdfc3163f854cf4ead55cbdfa
Augustine Alul <117871941+21Alul21@users.noreply.github.com>
2025-07-06 01:08:53 +0100

    Create UserRepository.java

---
+++ AppointmentManagement/src/main/java/com/example/AppointmentManagement/Repositories/UserRepository.java
@@ -0,0 +1,7 @@
+import com.example.demo.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<UserEntity, UUID> {
+    Optional<UserEntity> findByEmail(String email);
+}
