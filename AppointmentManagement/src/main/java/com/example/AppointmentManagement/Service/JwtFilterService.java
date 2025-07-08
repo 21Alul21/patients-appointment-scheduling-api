1fbbd4dfb9cc67160a403ee688b7b2225642ba5c
Augustine Alul <117871941+21Alul21@users.noreply.github.com>
2025-07-08 16:06:43 +0100

    Create JwtFilterService.java

---
+++ AppointmentManagement/src/main/java/com/example/AppointmentManagement/Service/JwtFilterService.java
@@ -0,0 +1,28 @@
+@Component
+@RequiredArgsConstructor
+public class JwtFilter extends OncePerRequestFilter {
+    private final JwtUtil jwt;
+    private final CustomUserDetailsService uds;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest req, HttpServletResponse res, FilterChain chain)
+            throws ServletException, IOException {
+
+        String header = req.getHeader("Authorization");
+        String token = null, email = null;
+        if (header != null && header.startsWith("Bearer ")) {
+            token = header.substring(7);
+            email = jwt.extractEmail(token);
+        }
+        if (email != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails ud = uds.loadUserByUsername(email);
+            if (jwt.validateToken(token, ud)) {
+                UsernamePasswordAuthenticationToken auth =
+                    new UsernamePasswordAuthenticationToken(ud, null, ud.getAuthorities());
+                auth.setDetails(new WebAuthenticationDetailsSource().buildDetails(req));
+                SecurityContextHolder.getContext().setAuthentication(auth);
+            }
+        }
+        chain.doFilter(req, res);
+    }
+}
