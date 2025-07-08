ed0cfe8526726f914997990d5dd0dee1c41a0056
Augustine Alul <117871941+21Alul21@users.noreply.github.com>
2025-07-08 16:04:01 +0100

    Update CustomUserDetailsService.java

--- AppointmentManagement/src/main/java/com/example/AppointmentManagement/Service/CustomUserDetailsService.java
+++ AppointmentManagement/src/main/java/com/example/AppointmentManagement/Service/CustomUserDetailsService.java
@@ -1,6 +1,13 @@
-
 @Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository repo;
 
-piblic class CustomUserDetailsService{
-  
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        UserEntity u = repo.findByEmail(email)
+            .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return new User(u.getEmail(), u.getPassword(),
+            List.of(new SimpleGrantedAuthority("ROLE_" + u.getRole().name())));
+    }
 }
