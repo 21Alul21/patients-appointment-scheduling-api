62fe827be72e2c4fdc5ef9665acf5f5c01d48c42
Augustine Alul <117871941+21Alul21@users.noreply.github.com>
2025-07-08 16:19:52 +0100

    Create SecurityConfig.java

---
+++ AppointmentManagement/src/main/java/com/example/AppointmentManagement/Service/SecurityConfig.java
@@ -0,0 +1,34 @@
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    private final CustomUserDetailsService uds;
+    private final JwtFilter jwtFilter;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(cs -> cs.disable())
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/api/v1/**").permitAll()
+                .requestMatchers("/api/v1/admin/**").hasRole("ADMIN")
+                .requestMatchers("/api/v1/doctor/**").hasRole("DOCTOR")
+                .requestMatchers("/api/v1/patient/**").hasRole("PATIENT")
+                .anyRequest().authenticated()
+            )
+            .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
