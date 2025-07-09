6f7c29d99fd70fe4b313f37c74f94273bdca766d
Augustine Alul <117871941+21Alul21@users.noreply.github.com>
2025-07-09 15:17:20 +0100

    Update UserService.java

--- AppointmentManagement/src/main/java/com/example/AppointmentManagement/Service/UserService.java
+++ AppointmentManagement/src/main/java/com/example/AppointmentManagement/Service/UserService.java
@@ -1,6 +1,18 @@
 @Service
 public class UserService{
-  public UserEntity registerUser(RegisterDTO){
- 
+  public UserEntity registerUser(RegisterDTO registerDTO){
+     if (registerDTO == null){
+       throw new IllegalArgumentException("user registration fields cannot be empty");
+     }
+    
+    OrganizationEntity organization = organizationRepository.findById(registerDTO.getOrganizationId)
+      .orElseThrow(new OrganizationNotFoundException("the organization id provided during regustration is invalid");
   }
+
+  UserEntity user = new UserEntity();
+  user.setEmail(registerDTO.getEmail());
+  user.setPassword(registerDTO.getPassward());
+  user.setRole(registerDTO.getRole());
+
+ // bi-directional relationship assignment 
 }
