83291d4c4e9bbd8db76690054f82d8a1677f5577
Augustine Alul <117871941+21Alul21@users.noreply.github.com>
2025-07-10 10:10:24 +0100

    Update PatientController.java

--- AppointmentManagement/src/main/java/com/example/AppointmentManagement/Controller/PatientController.java
+++ AppointmentManagement/src/main/java/com/example/AppointmentManagement/Controller/PatientController.java
@@ -8,21 +8,32 @@ public class PatientController {
         this.patientService = patientService;
     }
 
+    // SUPERADMIN can get a patient 
     @GetMapping("/{patientId}")
     public ResponseEntity<PatientDTO> getPatient(@PathVariable UUID patientId) {
         return ResponseEntity.ok(patientService.getPatient(patientId));
     }
 
+    // SUPERADMIN can get all patients across orgs
     @GetMapping("/patients-list")
     @public ResponseEntity<List<PatientDTO>> getPatients(){
     return ResponseEntity.ok(patientService.getPatients());
     }
 
+    // ORGADMINS can get list of patients in their org
+    @GetMapping("/org-patients-list")
+    public ResponseEntity<List<PatientDTO>> getOrgPatients(){
+      return ResponseEntity
+          .ok(patientService.orgPatients());  
+    }
+
+    // SUPERADMIN can update any patient record across orgs
     @PatchMapping("/update-profile/{patientId}")
     public ResponseEntity<PatientEntity> updatePatientProfile(@Valid @RequestBody PatientEntity patientEntity, @PathVariable UUID patientId){
     return ResponseEntity.ok(patientService.updatePatient(patientEntity, patientId));
     }
 
+     // SUPERADMIN can delete any patient record across orgs
     @DeleteMapping("/delete-patient/{patientId}")
     public ResponseEntity<Void> deletePatient(@PathVariable UUID patientId) {
     patientService.deletePatient(patientId);
