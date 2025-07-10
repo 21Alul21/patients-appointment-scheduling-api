6f9c6d1948b396e8558a0053c54d56e455be8774
Augustine Alul <117871941+21Alul21@users.noreply.github.com>
2025-07-10 10:58:20 +0100

    Update DoctorController.java

--- AppointmentManagement/src/main/java/com/example/AppointmentManagement/Controller/DoctorController.java
+++ AppointmentManagement/src/main/java/com/example/AppointmentManagement/Controller/DoctorController.java
@@ -7,21 +7,35 @@ public class DoctorController{
   public DoctorController(DoctorService doctorService){
     this.doctorService = doctorService;
   }
+
+  // SUPERADMIN can get a doctor
   @GetMapping("/{doctorId}")
   public ResponseEntity<DoctorDTO> getDoctor(@PathVariable UUID doctorId){
-    return ResponseEntity.ok(doctorService.getDoctor(doctorId));
+    return ResponseEntity
+      .ok(doctorService.getDoctor(doctorId));
   }
 
+  // SUPERADMIN can get all doctors across orgs
   @GetMapping("/all")
   public ResponseEntity<List<DoctorDTO>> getDoctors(){
-    return ResponseEntity.ok(doctorService.getDoctors());
+    return ResponseEntity
+      .ok(doctorService.getDoctors());
   }
 
+  // ORGADMINS can get list of doctors in their org
+    @GetMapping("/org-patients-list")
+    public ResponseEntity<List<DoctorDTO>> getOrgPatients(){
+      return ResponseEntity
+        .ok(doctorService.getOrgDoctors());  
+    }
+
+  // SUPERADMIN can update a doctor record
   @PatchMapping("/update-profile/{doctorId}")
   public ResponseEntity<DoctorEntity> updateDoctor(@RequestBody DoctorEntity doctorEntity, @PathVariable UUID doctorId){
    return ResponseEntity.ok(doctorService.updateDoctor(doctorEntity, doctorId));
   }
 
+  // SUPERADMIN can delete any doctor record across orgs
   @DeleteMapping("/delete-account/{doctorId}")
   public ResponseEntity<Void> deleteDoctor(@PathVariable UUID doctorId){
     doctorService.deleteDoctor(doctorId);
