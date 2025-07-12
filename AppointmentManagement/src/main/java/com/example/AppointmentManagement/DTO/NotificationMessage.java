64fdce3aaa7c7a287e1e8ef6a299dc54a56e03e3
Augustine Alul <117871941+21Alul21@users.noreply.github.com>
2025-07-12 18:24:08 +0100

    Create NotificationMessage.java

---
+++ AppointmentManagement/src/main/java/com/example/AppointmentManagement/DTO/NotificationMessage.java
@@ -0,0 +1,9 @@
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class NotificationMessage {
+    private String message;
+    private String sender;
+    private String appointmentId;
+    private String timestamp;
+}
