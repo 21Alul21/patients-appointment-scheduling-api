426a97a92d12001ffe100616caa9bb03d94ef3e2
Augustine Alul <117871941+21Alul21@users.noreply.github.com>
2025-07-12 18:20:31 +0100

    Create WebSocketConfig.java

---
+++ AppointmentManagement/src/main/java/com/example/AppointmentManagement/Service/WebSocketConfig.java
@@ -0,0 +1,17 @@
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws") 
+                .setAllowedOriginPatterns("*")
+                .withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/topic", "/queue");
+        registry.setApplicationDestinationPrefixes("/app");
+        registry.setUserDestinationPrefix("/user"); // for private messages
+    }
+}
