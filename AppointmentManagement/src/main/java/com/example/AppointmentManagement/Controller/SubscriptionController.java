7d10c7111a66f1345ae24420351edb1c579b3066
Augustine Alul <117871941+21Alul21@users.noreply.github.com>
2025-07-14 15:53:55 +0100

    Update SubscriptionController.java

--- AppointmentManagement/src/main/java/com/example/AppointmentManagement/Controller/SubscriptionController.java
+++ AppointmentManagement/src/main/java/com/example/AppointmentManagement/Controller/SubscriptionController.java
@@ -1,3 +1,13 @@
 @RestController
+@RequestMapping("/org-admin")
 public class SubscriptionController{
+  
+  
+  @PostMapping("/subscribe")
+  public ResponseEntity<?> orgSubscription(@RequestMapping SubscriptionDTO subscriptionDTO){
+   // get current request and confirm if it’s the admin
+     UserEntity currentUser = jwtUtils.authenticateUser();
+     UUID orgId =  currentUser.getOrganization().getOrganizationId();
+    return ResponseEntity.ok() 
+  }
 }
