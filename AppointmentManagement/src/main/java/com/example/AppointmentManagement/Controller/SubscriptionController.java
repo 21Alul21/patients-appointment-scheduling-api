f80ba750ee80a265c22e26dda2a67828603c1e56
Augustine Alul <117871941+21Alul21@users.noreply.github.com>
2025-07-14 19:24:42 +0100

    Update SubscriptionController.java

--- AppointmentManagement/src/main/java/com/example/AppointmentManagement/Controller/SubscriptionController.java
+++ AppointmentManagement/src/main/java/com/example/AppointmentManagement/Controller/SubscriptionController.java
@@ -3,14 +3,19 @@
 public class SubscriptionController{
   
   
-  @PostMapping("/subscribe")
+  @GetMapping("/trial-subscribtion")
   public ResponseEntity<?> orgSubscription(@RequestMapping SubscriptionDTO subscriptionDTO){
    // get current user request and confirm if it’s the admin
      UserEntity currentUser = jwtUtils.authenticateUser();
      OrganizationEntity organization =  currentUser.getOrganization();
-     // process the payment request
-
+  // check if the organization has already used trial version before
+  if (organization.isTrial == true){
+    throw new RuntimeException("You can no longer use a trial subscription");
+  }
+  organization.setIstrial() = true;
+  organization.setSubscriptionDuration(5);
+   
     // set the organization entity
-    return ResponseEntity.ok() 
+    return ResponseEntity.ok(Map.of("message" : "your trial version has been activated"));
   }
 }
