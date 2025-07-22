f4bf7d52d7774a2068230c9e96c8f55c355e0b18
Augustine Alul <117871941+21Alul21@users.noreply.github.com>
2025-07-22 22:45:01 +0100

    Update and rename RegistrationDTO.java to RegistraterDTO.java

--- AppointmentManagement/src/main/java/com/api/AppointmentManagement/DTO/RegistraterDTO.java
+++ AppointmentManagement/src/main/java/com/api/AppointmentManagement/DTO/RegistraterDTO.java
@@ -3,7 +3,7 @@ package com.api.AppointmentManagement.DTO
 @Data
 @AllArgsConstructor
 @NoArgsConstrutor
-public class RegistrationDTO{
+public class RegisterDTO{
   private UUID orgRegNumber;
   private String email;
   private String password;
