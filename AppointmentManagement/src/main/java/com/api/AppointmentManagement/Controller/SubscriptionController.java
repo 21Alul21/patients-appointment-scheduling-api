e0ac5bb3a071bbbcd3e7f0871e157ca081c88f7a
Augustine Alul <117871941+21Alul21@users.noreply.github.com>
2025-07-24 22:48:12 +0100

    Update SubscriptionController.java

--- AppointmentManagement/src/main/java/com/api/AppointmentManagement/Controller/SubscriptionController.java
+++ AppointmentManagement/src/main/java/com/api/AppointmentManagement/Controller/SubscriptionController.java
@@ -1,17 +1,20 @@
 package com.api.AppointmentManagement.Controller;
 
 @RestController
-@RequestMapping("/org-admin")
+@RequestMapping("api/v1/org-admin")
 @RequiredAllArgsConstructor
 public class SubscriptionController{
-  private final JwtUtils jwtUtils;
+  private final JwtUtil jwtUtil;
   private final OrganizationRepository organizationRepository;
   
  @GetMapping("/trial-subscription")
   public ResponseEntity<?> orgSubscription(){
    // get current user request and confirm if it’s the admin
-     UserEntity currentUser = jwtUtils.authenticateUser();
-     OrganizationEntity organization =  currentUser.getOrganization();
+     Optional<UserEntity> currentUser = jwtUtil.getCurrentUser();
+      if (!currentUser.isEmpty()){
+        UserEntity authUser = currentUser.get();
+      }
+     OrganizationEntity organization =  authUser.getOrganization();
   // check if the organization has already used trial version before
   if (!organization.isTrial){
     throw new RuntimeException("You can no longer use a trial subscription");
@@ -23,4 +26,8 @@ public class SubscriptionController{
    
     return ResponseEntity.ok(Map.of("message", "your trial version has been activated for 5 days"));
   }
+
+  @PostMapping("pay-subscription")
+  
+  //implement subscription logic here
 }
